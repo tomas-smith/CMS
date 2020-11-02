3e4b9cc275715b01c98c4c29ddbb4869fb22c2f6
tomas-smith <66832482+tomas-smith@users.noreply.github.com>
2020-11-02 12:40:48 +0000

    Initial commit

---
+++ includes/db.php
@@ -0,0 +1,9 @@
+<?php
+//connecting to the server
+$connection = mysqli_connect("localhost", "root", "", "cms");
+
+//confirming connection
+if($connection){
+    //echo "Connection status: confirmed ";
+}
+?>
